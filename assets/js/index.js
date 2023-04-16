b10f0a98ecee25f718db3d9db08012c4abcc4218
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-17 00:58:02 +0300

    + visibility on scroll position

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+const $el = document.getElementById('please-scroll-down');
+let fadeEffect;
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+        // If the user has scrolled down, fade out the element
+        clearInterval(fadeEffect);
+        fadeEffect = setInterval(function() {
+            if (!$el.style.opacity) {
+                $el.style.opacity = 1;
+            }
+            if ($el.style.opacity > 0) {
+                $el.style.opacity -= 0.1;
+            } else {
+                clearInterval(fadeEffect);
+                $el.style.display = 'none';
+            }
+        }, 50);
+    } else {
+        // If the user is at the top of the page, fade in the element
+        clearInterval(fadeEffect);
+        $el.style.opacity = 1;
+        $el.style.display = 'block';
+    }
+});
