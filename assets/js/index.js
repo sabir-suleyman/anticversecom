8fa832412b688c501bf6e80070c6c71ee3fdcdc5
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-29 18:57:12 +0300

    cookies... just this once ;)

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,28 @@ function closeAnnouncement() {
     $announcement.style["display"] = "none";
     $announcement.remove();
 }
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Check for the cookie when the page loads
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 window.addEventListener('scroll', function() {
     if (window.scrollY > 0) {
         // If the user has scrolled down, fade out the element
